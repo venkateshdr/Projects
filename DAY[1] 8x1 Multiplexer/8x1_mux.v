be867c51120c6c9d4c16c803afd0437d0dde9004
venkateshdr <144128924+venkateshdr@users.noreply.github.com>
2024-01-19 12:22:26 +0530

    Delete DAY[1] 8x1 Multiplexer directory

--- DAY[1] 8x1 Multiplexer/8x1_mux.v
+++
@@ -1,6 +0,0 @@
-module mux(i,s,y);
-input[7:0] i;
-input[2:0] s;
-output y;
-assign y = (s==3'b000)?i[0]:((s==3'b001)?i[1]:((s==3'b010)?i[2]:((s==3'b011)?i[3]:((s==3'b100)?i[4]:((s==3'b101)?i[5]:((s==3'b110)?i[6]:((s==3'b111)?i[7]:1'bx)))))));
-endmodule
