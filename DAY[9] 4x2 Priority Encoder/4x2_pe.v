efadd694e5242f96cae5bf5e8228d6b4f93a5033
venkateshdr <144128924+venkateshdr@users.noreply.github.com>
2024-01-19 12:24:51 +0530

    Delete DAY[9] 4x2 Priority Encoder directory

--- DAY[9] 4x2 Priority Encoder/4x2_pe.v
+++
@@ -1,6 +0,0 @@
-module pe(i,y);
-input[3:0] i;
-output[1:0] y;
-assign y[1]=i[2]|i[3];
-assign y[0]=i[3]|((~i[2])&i[1]);
-endmodule
--- DAY[9] 4x2 Priority Encoder/gate_4x2_pe.v
+++
@@ -1,9 +0,0 @@
-module pe(i,y);
-input[3:0] i;
-output[1:0] y;
-wire n1,n2,n3,n4;
-or  (n1,i[2],i[3]);
-not (n4,i[2]);
-and (n2,i[1]);
-or  (n3,i[3],n2);
-endmodule
