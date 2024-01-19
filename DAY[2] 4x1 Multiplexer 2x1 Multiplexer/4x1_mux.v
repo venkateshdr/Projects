02a99cafdb1d74e334e65480cc0721dbdc6fd3a6
venkateshdr <144128924+venkateshdr@users.noreply.github.com>
2024-01-19 12:22:44 +0530

    Delete DAY[2] 4x1 Multiplexer 2x1 Multiplexer directory

--- DAY[2] 4x1 Multiplexer 2x1 Multiplexer/4x1_mux.v
+++
@@ -1,6 +0,0 @@
-module mux(i,s,y);
-input[3:0] i;
-input[1:0] s;
-output y;
-assign y = (s==2'b00) ?i[0]: ( (s==2'b01)? i[1]: ((s==2'b10)?i[2]:( (s==2'b11)?i[3] :1'bx )));
-endmodule
--- DAY[2] 4x1 Multiplexer 2x1 Multiplexer/behav_2x1_mux.v
+++
@@ -1,9 +0,0 @@
-module mux(i0,i1,s,y);
-input i0,i1,s;
-output reg y;
-always@(*)begin
-y=s?i1:i0;
-//if(s) y=i1;
-//else y=i0;
-end
-endmodule
